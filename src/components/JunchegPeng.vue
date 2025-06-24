3e86472c9775e395a85cef22b0737e6e359a6f3b
彭俊城 <10060936+apeng-juncheng@user.noreply.gitee.com>
2025-06-24 20:03:59 +0800

    Initial commit

---
+++ src/components/JunchegPeng.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome">
+    <h1>欢迎回来，{{ userName }}</h1>
+  </div>
+</template>
+<style>
+    .welcome{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<script setup>
+import { ref } from 'vue'
+const userName = ref('Juncheng Peng')
+</script>
