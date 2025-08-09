dd38d96ddc57b8925ab712e800f905e459edb617
彭俊城 <10060936+apeng-juncheng@user.noreply.gitee.com>
2025-08-09 19:57:13 +0800

    Lab11-Configure deployment workflow and base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FIT5032_2025_Juncheng_Peng_35523107/',
   plugins: [
     vue(),
     vueDevTools(),
